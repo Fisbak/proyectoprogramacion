d72609e142aa39c8f267f20865cc204d102ca499
Franco <francotorres0209@gmail.com>
2025-06-06 16:33:34 -0300

    main sin terminar

--- soft.cpp
+++ soft.cpp
@@ -5,11 +5,31 @@ using namespace std;
 
 
 int main(){
-    int CodigoDePedido[100], DNIResponsable[100];
+    int CodigoDePedido[100], DNIResponsable[100], i=0, j=0;
     float PesoDelPedido[100], CostoDelPedido[100];
-    string ProvinciaDestino[100], Responsable[100], Provinciarecibe
+    string ProvinciaDestino[100], Responsable[100], Provinciarecibe[100];
 
 
+    while(i=0){
+        cout<<"Ingrese la provincia de la fabrica que recibe el pedido del pedido "<<j+1<<endl;
+        cin>>Provinciarecibe[j];
+
+        cout<<"Ingrese el codigo del pedido "<<j+1<<endl;
+        cin>>CodigoDePedido[j];
+
+        cout<<"Ingrese el peso(en kilogramos) del pedido "<<j+1<<endl;
+        cin>>PesoDelPedido[j];
+
+        cout<<"Ingrese la provincia a la que debe llegar el pedido "<<j+1<<endl;
+        cin>>CodigoDePedido[j];
+
+
+
+
+
+
+
+    }
 
 
 
